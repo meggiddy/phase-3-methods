5dd627ce9307c0bdf3cde514f8ee19603aa6b72a
meggidy <mimamitchelle@gmail.com>
2023-02-08 09:58:34 +0300

    Completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(a, b)
+  return (a + b)
+end
+
+def halve(c)
+  if c.is_a? Integer
+    return c / 2
+  else
+    return nil
+  end
+end
